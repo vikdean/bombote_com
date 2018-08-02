a70e070e8b9eb756d5090a13f3d11681e12db6f9
Viktor <16238514+vikdean@users.noreply.github.com>
2018-08-02 14:38:05 +0200

    Create sdns.py

---
+++ python/sdns.py
@@ -0,0 +1,37 @@
+#!/tmp/vik/python/bin/python3
+
+import os
+import signal
+import sys
+import csv
+import subprocess
+
+signal.signal(signal.SIGPIPE, signal.SIG_DFL)
+signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+
+def get_input():
+    global hosts
+    print("\n")
+    print("\x1b[6;30;43m" + "Enter the hostnames line-by-line. Use Ctrl-D to save it." + "\x1b[0m")
+    hosts = []
+    while True:
+        try:
+            line = input()
+        except EOFError:
+            break
+        hosts.append(line)
+    get_match(hosts)
+
+
+def get_match(match):
+    for i in range(len(hosts)):
+        match = subprocess.check_output(['grep', hosts[i], '/home/arlettv/scripts/bfg_node.csv'])
+        print(match)
+        #data = {}
+        #data['hostname'] = match.split(',')[0]
+        #data['ip'] = match.split(',')[1]
+        #print('%s') % data['hostname']
+
+
+get_input()
