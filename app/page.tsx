545b8b72d2cec9f234028e8c7af330c5c12bb568
Viktor Arlett <16238514+vikdean@users.noreply.github.com>
2025-07-26 20:04:40 +0200

    refactor: replace fragrance table with landing page navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,82 +1,34 @@
-import { parseCSV, type Fragrance, getLastModifiedDate } from "@/utils/csvParser";
-import FragranceTable from "@/components/FragranceTable";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { CircleAlert } from "lucide-react";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import Link from "next/link";
+import Image from "next/image";
 
 export const metadata = {
-  title: "Bombote - Perfume Reviews & Ratings Database",
-  description: "A comprehensive, ever-growing database of perfume reviews and ratings, featuring in-depth fragrance analysis, scent breakdowns, longevity and silage insights, and recommendations. Updated regularly to reflect the latest releases and timeless classics, helping fragrance enthusiasts discover their perfect scent.",
+  title: "Bombote.com",
+  description: "Perfume, Movie and TV Show ratings",
 };
 
 export default function Home() {
-  const fragrances: Fragrance[] = parseCSV();
-
   return (
-    <div className="bg-secondary" aria-label="Fragrance Reviews Database">
+    <div className="bg-secondary">
       <div className="max-w-[1920px] mx-auto pt-12 px-4 sm:px-6 lg:px-8">
-        <h1 className="text-2xl md:text-3xl font-bold text-foreground">Perfume Ratings & Reviews</h1>
-      </div>
-      <div className="max-w-[1920px] mx-auto pb-6 sm:px-6 lg:px-8">
-        <div className="px-4 py-2 sm:px-0">
-          <div className="grid grid-cols-1 md:grid-cols-[230px_230px_1fr] gap-4 my-4">
-            <Card className="shadow-2xl bg-muted-foreground/20 border hover:border-emerald-500 w-full">
-              <CardHeader>
-                <CardTitle className="pb-4 text-sm md:text-base">Fragrances reviewed</CardTitle>
-                <CardDescription className="text-2xl md:text-3xl font-bold text-emerald-400">{fragrances.length}</CardDescription>
-              </CardHeader>
-            </Card>
-
-            <Card className="shadow-2xl bg-muted-foreground/20 border hover:border-emerald-500 w-full">
-              <CardHeader>
-                <CardTitle className="pb-4 text-sm md:text-base">Last update</CardTitle>
-                <CardDescription className="text-lg md:text-xl font-bold text-emerald-400">{getLastModifiedDate()}</CardDescription>
-              </CardHeader>
-            </Card>
-
-            <Card className="shadow-2xl bg-muted-foreground/20 border hover:border-emerald-500 w-full">
-              <CardContent>
-                <div className="flex items-center space-x-4 rounded-md pt-6">
-                  <CircleAlert />
-                  <div className="flex-1 space-y-1">
-                    <CardTitle className="text-xs md:text-sm font-bold leading-none text-foreground">Note</CardTitle>
-                    <p className="text-xs md:text-sm text-gray-300">
-                      Please keep in mind, the ratings on this page are strictly based on my own taste and preferences. Most of these were tested with genuine 2ml sample tubes.
-                    </p>
-                  </div>
-                </div>
-              </CardContent>
-            </Card>
-          </div>
-          {/* Client-side interactive table */}
-          <div className="block">
-            <FragranceTable fragrances={fragrances} />
-          </div>
-
-          {/* Server-rendered table for SEO*/}
-          <div className="sr-only">
-            <h2>Perfume Database</h2>
-            <Table>
-              <TableHeader>
-                <TableRow>
-                  {Object.keys(fragrances[0]).map((key) => (
-                    <TableHead key={key}>{key}</TableHead>
-                  ))}
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {fragrances.map((fragrance, index) => (
-                  <TableRow key={index}>
-                    {Object.entries(fragrance).map(([key, value]) => (
-                      <TableCell key={key}>{key === "Link to buy" ? <Link href={value}>Amazon Link</Link> : value}</TableCell>
-                    ))}
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </div>
-        </div>
+        <Image src="/logo.png" alt="Logo" width="300" height="200" className="justify-center" />
+        <a href="#_" className="relative p-0.5 inline-flex items-center justify-center font-bold overflow-hidden group rounded-md">
+          <span className="w-full h-full bg-gradient-to-br from-[#ff8a05] via-[#ff5478] to-[#ff00c6] group-hover:from-[#ff00c6] group-hover:via-[#ff5478] group-hover:to-[#ff8a05] absolute"></span>
+          <span className="relative px-6 py-3 transition-all ease-out bg-gray-900 rounded-md group-hover:bg-opacity-0 duration-400">
+            <span className="relative text-white">Perfume Ratings</span>
+          </span>
+        </a>
+        <a href="#_" className="relative p-0.5 inline-flex items-center justify-center font-bold overflow-hidden group rounded-md">
+          <span className="w-full h-full bg-gradient-to-br from-[#ff8a05] via-[#ff5478] to-[#ff00c6] group-hover:from-[#ff00c6] group-hover:via-[#ff5478] group-hover:to-[#ff8a05] absolute"></span>
+          <span className="relative px-6 py-3 transition-all ease-out bg-gray-900 rounded-md group-hover:bg-opacity-0 duration-400">
+            <span className="relative text-white">Movie Ratings</span>
+          </span>
+        </a>
+        <a href="#_" className="relative p-0.5 inline-flex items-center justify-center font-bold overflow-hidden group rounded-md">
+          <span className="w-full h-full bg-gradient-to-br from-[#ff8a05] via-[#ff5478] to-[#ff00c6] group-hover:from-[#ff00c6] group-hover:via-[#ff5478] group-hover:to-[#ff8a05] absolute"></span>
+          <span className="relative px-6 py-3 transition-all ease-out bg-gray-900 rounded-md group-hover:bg-opacity-0 duration-400">
+            <span className="relative text-white">TV Show Ratings</span>
+          </span>
+        </a>
       </div>
     </div>
   );
