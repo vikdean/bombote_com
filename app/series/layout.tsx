2b23eecb086b41ced6ea5f0f21a45a2901780751
Viktor Arlett <16238514+vikdean@users.noreply.github.com>
2025-07-27 01:03:47 +0200

    feat: add series layout component

---
+++ app/series/layout.tsx
@@ -0,0 +1,46 @@
+import "@/app/globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Navigation from "@/components/SeriesNavigation";
+import Footer from "@/components/SeriesFooter";
+import Analytics from "@/components/Analytics";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Bombote.com",
+  description: "Perfume, Movie and TV Show ratings",
+  keywords:
+    "movie ratings, series ratings, tv show ratings, fragrance reviews, perfume ratings, best perfumes, cologne reviews, top fragrances, perfume blog, niche fragrances, designer perfumes, fragrance recommendations, perfume guide, unisex perfumes, luxury perfumes, fragrance notes, Eau de Parfum reviews, cologne guide, perfume comparisons, top-rated perfumes, affordable perfumes, mens cologne",
+  openGraph: {
+    title: "Bombote.com",
+    description: "Perfume, Movie and TV Show ratings",
+    type: "website",
+    locale: "en_US",
+    siteName: "Bombote.com",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Bombote.com",
+    description: "Perfume, Movie and TV Show ratings",
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" className="dark">
+      <body className={inter.className}>
+        <Analytics />
+        <div className="flex flex-col min-h-screen">
+          <Navigation />
+          <main className="flex-grow bg-secondary">{children}</main>
+          <Footer />
+        </div>
+      </body>
+    </html>
+  );
+}
