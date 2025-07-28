4e6f06e793be81ebeff9542cf3ca75242eccc20b
Viktor Arlett <16238514+vikdean@users.noreply.github.com>
2025-07-29 00:16:33 +0200

    fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,24 @@
-import { FlatCompat } from '@eslint/eslintrc'
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const compat = new FlatCompat({
-  // import.meta.dirname is available after Node.js v20.11.0
-  baseDirectory: import.meta.dirname,
-})
+  baseDirectory: __dirname,
+});
+
 const eslintConfig = [
-  ...compat.config({
-    extends: ['next'],
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
     rules: {
-      'react/no-unescaped-entities': 'off',
+      "react/no-unescaped-entities": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "off",
     },
-  }),
-]
-export default eslintConfig
+  },
+];
+
+export default eslintConfig;
